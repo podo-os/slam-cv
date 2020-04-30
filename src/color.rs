4201ec2b2ba40c0449ffa8361a51ce8926efe82d
h <besqer996@gnu.ac.kr>
2020-05-01 08:01:03 +0900

    Add more colors

--- src/color.rs
+++ src/color.rs
@@ -7,4 +7,8 @@ impl Colors {
     pub fn red() -> RgbColor {
         RgbColor::new(1.0, 0.0, 0.0)
     }
+
+    pub fn white() -> RgbColor {
+        RgbColor::new(1.0, 1.0, 1.0)
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![feature(associated_type_defaults)]
 #![feature(generic_associated_types)]
 
-pub mod color;
+mod color;
 pub mod feature;
 pub mod frame;
 pub mod matcher;
@@ -9,14 +9,14 @@ mod number;
 pub mod solver;
 pub mod vo;
 
-pub use number::Number;
+pub use self::color::Colors;
+pub use self::number::Number;
 
 pub mod prelude {
-    pub use super::color::Colors;
     pub use super::feature::*;
     pub use super::frame::*;
     pub use super::matcher::*;
     pub use super::solver::*;
     pub use super::vo::*;
-    pub use super::Number;
+    pub use super::{Colors, Number};
 }
