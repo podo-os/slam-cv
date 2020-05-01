f4860b49ad1bb3aca71b04e8d8408b5d5f17d879
h <besqer996@gnu.ac.kr>
2020-05-01 10:58:05 +0900

    Add Default in Number

--- src/number.rs
+++ src/number.rs
@@ -3,7 +3,7 @@ use core::fmt::Debug;
 use num::{Float, FromPrimitive};
 use simba::scalar::RealField;
 
-pub trait Number: RealField + Float + FromPrimitive + Copy + Debug + Sync {}
+pub trait Number: RealField + Float + FromPrimitive + Copy + Debug + Default + Sync {}
 
 impl Number for f32 {}
 impl Number for f64 {}
