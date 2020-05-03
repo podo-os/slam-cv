95b88c8444d7d4cb7260a7e32ce78c3d2c3c28cf
h <besqer996@gnu.ac.kr>
2020-05-03 15:17:39 +0900

    Update colors

--- src/color.rs
+++ src/color.rs
@@ -1,14 +1,26 @@
 pub struct Colors;
 
 // Red, Green, Blue
-type RgbColor = nalgebra::Point3<f32>;
+type RgbColor = [f32; 3];
 
 impl Colors {
-    pub fn red() -> RgbColor {
-        RgbColor::new(1.0, 0.0, 0.0)
+    pub fn black() -> RgbColor {
+        [0.0, 0.0, 0.0]
     }
 
     pub fn white() -> RgbColor {
-        RgbColor::new(1.0, 1.0, 1.0)
+        [1.0, 1.0, 1.0]
+    }
+
+    pub fn red() -> RgbColor {
+        [1.0, 0.0, 0.0]
+    }
+
+    pub fn green() -> RgbColor {
+        [0.0, 1.0, 0.0]
+    }
+
+    pub fn blue() -> RgbColor {
+        [0.0, 0.0, 1.0]
     }
 }
