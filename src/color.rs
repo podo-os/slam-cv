e3ac8995f83d9084e6a8215228cb3928a59d5705
h <besqer996@gnu.ac.kr>
2020-05-03 16:36:12 +0900

    Set color consts

--- src/color.rs
+++ src/color.rs
@@ -4,23 +4,23 @@ pub struct Colors;
 type RgbColor = [f32; 3];
 
 impl Colors {
-    pub fn black() -> RgbColor {
+    pub const fn black() -> RgbColor {
         [0.0, 0.0, 0.0]
     }
 
-    pub fn white() -> RgbColor {
+    pub const fn white() -> RgbColor {
         [1.0, 1.0, 1.0]
     }
 
-    pub fn red() -> RgbColor {
+    pub const fn red() -> RgbColor {
         [1.0, 0.0, 0.0]
     }
 
-    pub fn green() -> RgbColor {
+    pub const fn green() -> RgbColor {
         [0.0, 1.0, 0.0]
     }
 
-    pub fn blue() -> RgbColor {
+    pub const fn blue() -> RgbColor {
         [0.0, 0.0, 1.0]
     }
 }
